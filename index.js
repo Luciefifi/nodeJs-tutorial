eb18049749c9959cdd9bbaf3a4b729be3216ae7d
Luciefifi <niyomutonilucie@gmail.com>
2023-01-15 17:48:42 +0300

    ft(into): - core modules -common js module -create index ,math.js files [finishes #1]

---
+++ index.js
@@ -0,0 +1,26 @@
+console.log("nodeJs tutorial")
+
+const os = require("os")
+const path = require("path")
+const {add , subtract ,  multiply , divide}= require("./math")
+
+console.log(add(8,9))
+console.log(multiply(8,9))
+console.log(divide(8,9))
+console.log(subtract(8,9))
+
+/* some core modules
+
+
+console.log(os.type())
+console.log(os.version())
+console.log(os.homedir())
+
+console.log(__dirname)
+console.log(__filename)
+
+console.log(path.dirname(__filename))
+console.log(path.basename(__filename))
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))*/
