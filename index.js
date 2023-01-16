2eab9400e4074be60f731a033f0c32a13533fa8c
Luciefifi <niyomutonilucie@gmail.com>
2023-01-16 07:50:29 +0300

    ch(depndencies): - install nodemon - install node_modules - install date-nfs -install uuid -[finishes #003]

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 
-const fsPromises = require("fs").promises
-const path =require("path")
+// const fsPromises = require("fs").promises
+// const path =require("path")
 
-const fileOps = async () =>{
-    try{
+// const fileOps = async () =>{
+//     try{
         // const data = await fsPromises.readFile(path.join(__dirname , 'files' , 'starter.txt'), 'utf8')
         // console.log(data)
-        await fsPromises.writeFile(path.join(__dirname , 'files' , 'promiseWrite.txt'), "hello ")
-        await fsPromises.appendFile(path.join(__dirname , 'files' , 'promiseWrite.txt'), "\n nice to meet you")
-        await fsPromises.rename(path.join(__dirname , 'files' , 'newReply.txt'), path.join(__dirname , 'files' , 'newAStarter.txt'))
-        const newData =  await fsPromises.readFile(path.join(__dirname , 'files' , 'newAStarter.txt'), 'utf8')
-        console.log(newData)
+//         await fsPromises.writeFile(path.join(__dirname , 'files' , 'promiseWrite.txt'), "hello ")
+//         await fsPromises.appendFile(path.join(__dirname , 'files' , 'promiseWrite.txt'), "\n nice to meet you")
+//         await fsPromises.rename(path.join(__dirname , 'files' , 'newReply.txt'), path.join(__dirname , 'files' , 'newAStarter.txt'))
+//         const newData =  await fsPromises.readFile(path.join(__dirname , 'files' , 'newAStarter.txt'), 'utf8')
+//         console.log(newData)
        
 
-    }
-    catch(err){
-        console.error(err)
-    }
-}
-fileOps();
+//     }
+//     catch(err){
+//         console.error(err)
+//     }
+// }
+// fileOps();
 
 // fs.readFile( path.join(__dirname , 'files' , 'starter.txt'), 'utf8', (err , data)=> {
 //     if(err)throw err
@@ -50,3 +50,13 @@ fileOps();
 //     console.log(`there was an uncought error : ${err}`)
 //     process.exit(1)
 // })
+
+
+const {format } = require('date-fns')
+
+const {v4:uuid} = require('uuid')
+console.log(format(new Date() ,'yyyyMMMdd\tHH:mm:ss'))
+
+console.log("hello!!")
+console.log(uuid())
+console.log()
