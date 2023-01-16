137c37fee7c6ceebbfdc0a2de82b0f9e0f34a7de
Luciefifi <niyomutonilucie@gmail.com>
2023-01-16 06:16:26 +0300

    ft(filesystem): -read file -write file -append file -rename a file - delete a file -fs promises creat readStream create writestream create directory delete a directory [Finishes # 002]

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-console.log("nodeJs tutorial")
 
-const os = require("os")
-const path = require("path")
-const {add , subtract ,  multiply , divide}= require("./math")
+const fsPromises = require("fs").promises
+const path =require("path")
 
-console.log(add(8,9))
-console.log(multiply(8,9))
-console.log(divide(8,9))
-console.log(subtract(8,9))
+const fileOps = async () =>{
+    try{
+        // const data = await fsPromises.readFile(path.join(__dirname , 'files' , 'starter.txt'), 'utf8')
+        // console.log(data)
+        await fsPromises.writeFile(path.join(__dirname , 'files' , 'promiseWrite.txt'), "hello ")
+        await fsPromises.appendFile(path.join(__dirname , 'files' , 'promiseWrite.txt'), "\n nice to meet you")
+        await fsPromises.rename(path.join(__dirname , 'files' , 'newReply.txt'), path.join(__dirname , 'files' , 'newAStarter.txt'))
+        const newData =  await fsPromises.readFile(path.join(__dirname , 'files' , 'newAStarter.txt'), 'utf8')
+        console.log(newData)
+       
 
-/* some core modules
+    }
+    catch(err){
+        console.error(err)
+    }
+}
+fileOps();
+
+// fs.readFile( path.join(__dirname , 'files' , 'starter.txt'), 'utf8', (err , data)=> {
+//     if(err)throw err
+//     console.log(data)
+// })
+
+// fs.writeFile(path.join(__dirname , "files" , 'reply.txt',) , " nice to meet you" , err =>{
+//     if(err)throw err
+//     console.log("write completed !!")
+
+//     fs.appendFile(path.join(__dirname , "files", 'reply.txt'), " \n yes it is" , err=>{
+//         if(err) throw err
+//         console.log("append complete!!!")
 
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+//         fs.rename((path.join(__dirname , 'files' , 'reply.txt')) , path.join(__dirname , 'files' , 'newReply.txt') , err=>{
+//             if(err) throw err
+//             console.log("renaming complete!!!")
 
-console.log(__dirname)
-console.log(__filename)
+//     })
+//     })
+// })
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
 
-console.log(path.parse(__filename))*/
+
+// // exit on uncought error
+
+// process.on("uncaughtException" , (err )=> {
+//     console.log(`there was an uncought error : ${err}`)
+//     process.exit(1)
+// })
